93e3038db4f75fed3b3913c4f7050567b54187d3
sanghee01 <sang6299@naver.com>
2023-03-26 16:38:33 +0900

    Add: 배포

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const apiKey = "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";
+const serverless = require("serverless-http");
 const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
 var cors = require("cors");
@@ -10,11 +11,11 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 //CORS 이슈 해결
-// let corsOptions = {
-//     origin: 'https://www.domain.com',
-//     credentials: true
-// }
-app.use(cors());
+let corsOptions = {
+  origin: "https://chatdoge-sanghee.pages.dev",
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 //POST 요청 받을 수 있게 만듬
 app.use(express.json()); // for parsing application/json
@@ -84,4 +85,4 @@ app.post("/fortuneTell", async function (req, res) {
   res.json({ assistant: fortune });
 });
 
-app.listen(3000);
+module.exports.handler = serverless(app);
